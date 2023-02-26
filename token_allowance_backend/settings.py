3d02d7086060622b242c1d379969ffbfff693b28
Odiase <odiaseefosa18@gmail.com>
2023-02-26 19:50:40 +0100

    Hosted Static Files

--- token_allowance_backend/settings.py
+++ token_allowance_backend/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
     # third party packages
     "rest_framework",
+    "storages",
     
     # local apps
     "core.apps.CoreConfig"
@@ -136,11 +137,36 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_ROOT = Path(BASE_DIR)/'staticfiles'
+STATIC_URL = '/static/'
+ 
+### AWS S3 SETTING
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    Path(BASE_DIR)/'static'
-]
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'token-allowance'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_DEFAULT_ACL = None
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_HEADERS = {
+    'Access-Control-Allow-Origin' : '*',
+}
+AWS_LOCATION = 'static'
+# STATICFILES_DIRS = [
+#     Path(BASE_DIR)/"static"
+# # ]
+# aws static file storage
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_LOCATION = '/media/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, MEDIA_LOCATION)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
